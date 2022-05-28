cb1fa6158581c4808b815d3ec92f71b818654c6d
Vyazovetskov Vladimir <vyazovetckov_vs@magnit.ru>
2022-05-28 17:14:34 +0300

    Add README.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # sahibinden-bot
+
+<img align="left" src="img/demo.jpg">
+
 Bot that tracks new ads at sahibinden.com and notifies telegram channel
+
+## Configuration
+
+Sahibinden-bot can be configured using environment variables.
+The optimal way is to create a file named `.env` in the root `sahibinden_bot` directory.
+
+```bash
+SAHIBINDEN_SOURCE_URL='https://www.sahibinden.com/en/bicycles?address_town=655&address_city=48'
+
+TELEGRAM_BOT_TOKEN='bot_token'
+TELEGRAM_CHANNEL_ID='channel_id '
+
+AWS_ACCESS_KEY_ID='aws_access_key_id'
+AWS_SECRET_ACCESS_KEY='aws_access_key_id'
+AWS_BUCKET_NAME='aws_bucket_name'
+AWS_ENDPOINT_URL='https://s3.amazonaws.com'
+```
+
+SAHIBINDEN_SOURCE_URL: The URL of the search results page to scrape.
+
+Optional integration with Sentry.
+
+```bash
+SENTRY_DSN='sentry_dsn'
+```
+
+As a reliable way of requesting sahibinden.com, sahibinden-bot optionally uses a proxy.
+
+```bash
+PROXY_URL='http://user:pass@ip:port'
+PROXY_URL_BACKUP='http://user:pass@ip:port'
+```
--- sahibinden_bot/settings.py
+++ sahibinden_bot/settings.py
@@ -1,5 +1,5 @@
 import random
-from typing import Iterable
+from typing import Iterable, Optional
 
 from pydantic import BaseSettings
 
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     SAHIBINDEN_SOURCE_URL: str
     SAHIBINDEN_TIMEOUT: int = 10
 
-    PROXY_URL: str
-    PROXY_URL_BACKUP: str
+    PROXY_URL: Optional[str]
+    PROXY_URL_BACKUP: Optional[str]
 
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
@@ -19,14 +19,19 @@ class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str
     TELEGRAM_CHANNEL_ID: int
 
-    SENTRY_DSN: str
+    SENTRY_DSN: Optional[str]
 
     @property
-    def httpx_proxies(self) -> Iterable[dict]:
-        shuffled_proxies = [self.PROXY_URL, self.PROXY_URL_BACKUP]
-        random.shuffle(shuffled_proxies)
-        for proxy in shuffled_proxies:
+    def httpx_proxies(self) -> Iterable[Optional[dict]]:
+        proxies = []
+        if self.PROXY_URL:
+            proxies.append(self.PROXY_URL)
+        if self.PROXY_URL_BACKUP:
+            proxies.append(self.PROXY_URL_BACKUP)
+        random.shuffle(proxies)
+        for proxy in proxies:
             yield {"http://": proxy, "https://": proxy}
+        yield None
 
     @property
     def s3_credentials(self) -> dict:
